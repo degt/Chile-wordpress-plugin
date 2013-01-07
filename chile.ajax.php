57363e11dad27edba5850854910ca060293f12fd
Daniel Gutierrez <daniel.gutierrez@degt.cl>
2013-01-06 21:30:24 -0300

    Ahora obtiene datos desde MYSQL db

--- chile.ajax.php
+++ chile.ajax.php
@@ -4,21 +4,26 @@ add_action('init', 'initchile');
 
 function initchile(){
 	global $chile;
+	
+	if($_GET['region'] || $_GET['region'] === '0'){
+  	echo json_encode($chile->comunas($_GET['region']));
+  	exit;
+	}
 		
-	if($_GET['provincia'] != ''){
-		echo json_encode($chile->provincia($_GET['provincia']));
-		exit;
-	}
-	if($_GET['comuna'] != ''){
-		$dd = explode('-',$_GET['comuna']);
-		$region = $dd[0];
-		$provincia = $dd[1];
-		echo json_encode($chile->comuna($region,$provincia));
-		exit;
-	}
-	if($_GET['mycomuna'] || $_GET['mycomuna'] === "0"){
-		echo json_encode($chile->comunas( (int)$_GET['mycomuna'] ));
-		exit;
-	}
+	//if($_GET['provincia'] != ''){
+	//	echo json_encode($chile->provincia($_GET['provincia']));
+	//	exit;
+	//}
+	//if($_GET['comuna'] != ''){
+	//	$dd = explode('-',$_GET['comuna']);
+	//	$region = $dd[0];
+	//	$provincia = $dd[1];
+	//	echo json_encode($chile->comuna($region,$provincia));
+	//	exit;
+	//}
+	//if($_GET['comuna'] || $_GET['comuna'] === "0"){
+	//	echo json_encode($chile->comunas( (int)$_GET['comuna'] ));
+	//	exit;
+	//}
 }
  ?>
--- chile.php
+++ chile.php
@@ -9,100 +9,125 @@ Author URI: http://degt.cl
 */
 
 //var $chile as global
-$chile = new Chile;
-
 require("xml2array.php");
 require("chile.ajax.php");
+$chile = new Chile;
+
 
 class Chile{
-	public $opt;
-	
-	function Chile(){
-		$this->opt = array(
-			'url' => get_bloginfo('url').'/wp-content/plugins/chile/chile.xml'
-		);
-		
-	}
-	
-	function todos(){
-		//$contents = @file_get_contents($this->opt['url']);
-		//$result = xml2array($contents,1, 'attribute');
-		
-		$curl_handler = curl_init();
-		curl_setopt($curl_handler, CURLOPT_URL, $this->opt['url']);
-		curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curl_handler, CURLOPT_BINARYTRANSFER, 1); 
-		$f = curl_exec($curl_handler);
-		
-		$result = xml2array($f,1, 'attribute');
-		
-		return $result;
-	}
-	
-	function regiones($region = ''){
-		//Region:num
-		$arr = $this->todos();
-		foreach((array)$arr['chile']['region'] as $var){
-			$regiones[] = htmlentities($var['attr']['nombre'],ENT_QUOTES,'UTF-8');
-		}
-		if($region == ''){
-			return $regiones;
-		}else{
-			return $regiones[$region];
-		}
-	}
-	
-	function provincia($region, $provincia = ''){
-		$arr = $this->todos();
-		$i = 0;
-		foreach((array)$arr['chile']['region'][$region]['provincia'] as $var){
-			$provincias[$region.'-'.$i] = htmlentities($var['attr']['nombre'],ENT_QUOTES,'UTF-8');
-			$i++;
-		}
-		if($provincia == ''){
-			return $provincias;
-		}else{
-			return $provincias[$provincia];
-		}
-	}
-	
-	function comuna($region,$provincia,$comuna = ''){
-		$arr = $this->todos();
-		
-		$i = 0;
-		foreach((array)$arr['chile']['region'][$region]['provincia'][$provincia]['comuna'] as $var){
-			$comunas[$provincia.'-'.$i] = htmlentities($var['attr']['nombre'],ENT_QUOTES,'UTF-8');
-			$i++;
-		}
-		
-		if($comuna == ''){
-			return $comunas;
-		}else{
-			return $comunas[$comuna];
-		}
-	}
-	
-	/**
-	* Comunas de una region específica
-	*
-	* @param 
-	* @return 
-	*/ 
-	function comunas($region){
-		$arr = $this->todos();
-		
-		foreach( (array)$arr['chile']['region'][$region]['provincia'] as $var){
-			foreach($var['comuna'] as $v){
-			 if(!$v['attr']['nombre']) continue;
-				$comunas[] = $v['attr']['nombre'];
-			}
-		}
-		if(!$comunas) return false;
-		
-		sort($comunas);
-		return $comunas;
-	}
+  
+  function regiones(){
+    global $wpdb;
+    
+    $query = $wpdb->get_results("SELECT * FROM regiones");  
+    
+    return $query;
+  }
+    
+  function comunas($region_id){
+    global $wpdb;
+    
+    $query = $wpdb->get_results("SELECT * FROM comunas WHERE region_id = $region_id");
+    
+    return $query;
+
+  }
 }
+
+
+
+
+
+//class Chile{
+//	public $opt;
+//	
+//	function Chile(){
+//	   global $wpdb;
+//		$this->opt = array(
+//			'url' => get_bloginfo('url').'/wp-content/plugins/chile/chile.xml'
+//		);
+//		
+//	}
+//	
+//	function todos(){
+//		//$contents = @file_get_contents($this->opt['url']);
+//		//$result = xml2array($contents,1, 'attribute');
+//		
+//		$curl_handler = curl_init();
+//		curl_setopt($curl_handler, CURLOPT_URL, $this->opt['url']);
+//		curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, 1);
+//		curl_setopt($curl_handler, CURLOPT_BINARYTRANSFER, 1); 
+//		$f = curl_exec($curl_handler);
+//		
+//		$result = xml2array($f,1, 'attribute');
+//		
+//		return $result;
+//	}
+//	
+//	function regiones($region = ''){
+//		//Region:num
+//		$arr = $this->todos();
+//		foreach((array)$arr['chile']['region'] as $var){
+//			$regiones[] = htmlentities($var['attr']['nombre'],ENT_QUOTES,'UTF-8');
+//		}
+//		if($region == ''){
+//			return $regiones;
+//		}else{
+//			return $regiones[$region];
+//		}
+//	}
+//	
+//	function provincia($region, $provincia = ''){
+//		$arr = $this->todos();
+//		$i = 0;
+//		foreach((array)$arr['chile']['region'][$region]['provincia'] as $var){
+//			$provincias[$region.'-'.$i] = htmlentities($var['attr']['nombre'],ENT_QUOTES,'UTF-8');
+//			$i++;
+//		}
+//		if($provincia == ''){
+//			return $provincias;
+//		}else{
+//			return $provincias[$provincia];
+//		}
+//	}
+//	
+//	function comuna($region,$provincia,$comuna = ''){
+//		$arr = $this->todos();
+//		
+//		$i = 0;
+//		foreach((array)$arr['chile']['region'][$region]['provincia'][$provincia]['comuna'] as $var){
+//			$comunas[$provincia.'-'.$i] = htmlentities($var['attr']['nombre'],ENT_QUOTES,'UTF-8');
+//			$i++;
+//		}
+//		
+//		if($comuna == ''){
+//			return $comunas;
+//		}else{
+//			return $comunas[$comuna];
+//		}
+//	}
+//	
+//	/**
+//	* Comunas de una region específica
+//	*
+//	* @param 
+//	* @return 
+//	*/ 
+//	function comunas($region){
+//		$arr = $this->todos();
+//		
+//		foreach( (array)$arr['chile']['region'][$region]['provincia'] as $var){
+//			foreach($var['comuna'] as $v){
+//			 if(!$v['attr']['nombre']) continue;
+//				$comunas[] = $v['attr']['nombre'];
+//			}
+//		}
+//		if(!$comunas) return false;
+//		
+//		sort($comunas);
+//		return $comunas;
+//	}
+//}
 
 
 
